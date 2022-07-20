39a624762da031b5aa81406ac31d9052ef3a84eb
Gustav Schölin <gustav@scholin.dev>
2022-07-20 08:30:12 +0200

    Add README to 2022_06

--- puzzles/2022_06/2022_06.py
+++ puzzles/2022_06/2022_06.py
@@ -1,12 +1,4 @@
-"""Solution for Jane Street puzzle June 2022, Block Party 4
-
-Unfortunately the current solution only works in theory, the time complexity
-is too large. The example puzzle is solved instantly but the main puzzle isn't
-(8h on my laptop wasn't enough...).
-
-However, if I started solving the puzzle manually to reduce the search space
-the algorithm could easily do the rest when I got stuck.
-"""
+"""Solution for Jane Street puzzle June 2022, Block Party 4"""
 
 from collections import Counter
 
---
+++ puzzles/2022_06/README.md
@@ -0,0 +1,10 @@
+# Solution for Jane Street puzzle June 2022, Block Party 4
+
+https://www.janestreet.com/puzzles/block-party-4-index/
+
+Unfortunately the current solution only works in theory, the time complexity
+is too large. The example puzzle is solved instantly but the main puzzle isn't
+(8h on my laptop wasn't enough...).
+
+However, if I started solving the puzzle manually to reduce the search space
+the algorithm could easily do the rest when I got stuck.
